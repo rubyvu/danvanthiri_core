627379f1c0001c7dbe827cb67716c6ab991f99e4
Ngoc Vu <ngoc@digifutura.com>
2016-02-23 09:45:04 +0700

    move devise to api app

--- app/models/dhanvantri_core/patient.rb
+++ app/models/dhanvantri_core/patient.rb
@@ -1,9 +1,5 @@
 module DhanvantriCore
   class Patient < ActiveRecord::Base
-    # Include default devise modules. Others available are:
-    # :confirmable, :lockable, :timeoutable and :omniauthable
-    devise :database_authenticatable, :registerable,
-           :recoverable, :rememberable, :trackable, :validatable
 
     def generate_auth_token!
       begin
