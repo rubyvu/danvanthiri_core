1394e18c9e92f91fa770bf723d7cb122ea72ad93
Ruby Vu <rubyvu.vn@gmail.com>
2016-02-28 21:33:55 +0700

    connect facebook

--- app/models/danvanthiri_core/patient.rb
+++ app/models/danvanthiri_core/patient.rb
@@ -3,8 +3,8 @@ module DanvanthiriCore
     has_many :appointments, dependent: :destroy, foreign_key: "patient_id"
     has_many :social_credentials, dependent: :destroy, foreign_key: "patient_id"
 
-    validates :first_name, :last_name, :mobile_number, presence: true
-    validates :mobile_number, uniqueness: true
+    validates :first_name, :last_name, presence: true
+    validates :mobile_number, presence: true, uniqueness: true, on: :update
 
     scope :active, -> {where otp: nil}
     scope :inactive, -> {where.not otp: nil}
@@ -19,5 +19,37 @@ module DanvanthiriCore
       update_column :auth_token, nil
     end
   
+    def connect_facebook(token)
+      begin
+        @graph = Koala::Facebook::API.new(token)
+        profile = @graph.get_object("me")
+        if sc = SocialCredential.find_by uid: profile["id"], provider: "Facebook"
+          patient = sc.patient
+          return 1, patient
+        else
+          sc = SocialCredential.new(uid: profile["id"], provider: "Facebook")
+          patient = Patient.find_by_email profile["email"]
+          if patient
+            sc.patient_id = patient.id
+          else
+            gender = profile["gender"].capitalize if profile["gender"]
+            password = Devise.friendly_token.first(8)
+            patient = Patient.new(email: profile["email"], first_name: profile["first_name"], last_name: profile["last_name"],
+              gender: gender, password: password, password_confirmation: password)
+            patient.save(validate: false)
+          end
+          sc.patient_id = patient.if if patient
+          if sc.save
+            return 2, patient
+          else
+            return "INV", sc.errors.full_messages
+          end
+        end
+
+      rescue Exception => e
+        return "UN-AUTHOR", ["Invalid OAuth access token"]
+      end
+    end
+
   end
 end
