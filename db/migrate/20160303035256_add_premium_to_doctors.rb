c0bd210370245f190a7dbb12532e4ddcd12ea081
Ngoc Vu <ngoc@digifutura.com>
2016-03-03 12:13:29 +0700

    edit migration

--- db/migrate/20160303035256_add_premium_to_doctors.rb
+++ db/migrate/20160303035256_add_premium_to_doctors.rb
@@ -1,5 +1,6 @@
 class AddPremiumToDoctors < ActiveRecord::Migration
   def change
     add_column :danvanthiri_core_doctors, :premium, :boolean, default: false
+    add_column :danvanthiri_core_doctors, :banner, :string
   end
 end
