2844b9a4595d58a96aa22142c9c9b1251f502ebe
Ngoc Vu <ngoc@digifutura.com>
2016-03-23 10:46:34 +0700

    format time ampm

--- app/models/danvanthiri_core/available.rb
+++ app/models/danvanthiri_core/available.rb
@@ -3,15 +3,25 @@ module DanvanthiriCore
     enum week_day: [:sunday, :monday, :tuesday, :wednesday, :thursday, :friday, :saturday]
     belongs_to :doctor
     validates :time_from, :time_to, :week_day, presence: true
+
     def f_to_hour(number)
+     
+      h = number.to_i
+      ampm = "pm"
+      if h < 12
+        ampm = "am"
+      elsif h > 12
+        h = h-12
+      end
+      h = h < 10 ? "0#{h}" : h
+      
       arr = number.to_s.split(".")
-      h = arr.first.length < 2 ? "0#{arr.first}" : arr.first
       m = arr.last.length < 2 ? "#{arr.last}0" : arr.last
-      "#{h}:#{m}"
+      ampm_format "#{h}:#{m} #{ampm}"
     end
 
     def display_time
       "#{f_to_hour(time_from)} - #{f_to_hour(time_to)}"
-    end    
+    end 
   end
 end
