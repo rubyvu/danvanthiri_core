4c1794b994a1aec901b8b5e76940ce88a061a7b6
Ngoc Vu <ngoc@digifutura.com>
2016-03-29 13:56:40 +0700

    address helper for working location

--- app/models/danvanthiri_core/working_location.rb
+++ app/models/danvanthiri_core/working_location.rb
@@ -1,5 +1,8 @@
 module DanvanthiriCore
   class WorkingLocation < ActiveRecord::Base
     belongs_to :doctor
+    def address
+      [addr_street, addr_area, addr_city, addr_state].reject{|x| x.blank?}.join(', ')
+    end
   end
 end
