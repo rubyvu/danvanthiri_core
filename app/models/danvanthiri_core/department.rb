d42fa69b91f30551acd43becd02f323b223eb1e6
Ngoc Vu <ngoc@digifutura.com>
2016-04-21 09:27:04 +0700

    department - accept nested attributes for available

--- app/models/danvanthiri_core/department.rb
+++ app/models/danvanthiri_core/department.rb
@@ -2,7 +2,8 @@ module DanvanthiriCore
   class Department < ActiveRecord::Base
     belongs_to :hospital
     has_many :availables, as: :owner, dependent: :destroy
-    
+    accepts_nested_attributes_for :availables, allow_destroy: true
+
     validates :name, presence: true
   end
 end
