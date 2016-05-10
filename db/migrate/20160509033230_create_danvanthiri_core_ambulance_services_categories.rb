c5fd4518ebc6c89a5085a34adda69529c27fb33d
Ngoc Vu <ngoc@digifutura.com>
2016-05-10 10:33:13 +0700

    fix migration

--- db/migrate/20160509033230_create_danvanthiri_core_ambulance_services_categories.rb
+++ db/migrate/20160509033230_create_danvanthiri_core_ambulance_services_categories.rb
@@ -2,7 +2,7 @@ class CreateDanvanthiriCoreAmbulanceServicesCategories < ActiveRecord::Migration
   def change
     create_table :danvanthiri_core_ambulance_services_categories do |t|
       t.integer :ambulance_service_id
-      t.integer :ambelance_category_id
+      t.integer :ambulance_category_id
 
       t.timestamps null: false
     end
