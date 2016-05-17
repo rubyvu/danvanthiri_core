5eb19ffa8e2dac76a37bac0c17dd08570c21efaa
Ngoc Vu <ngoc@digifutura.com>
2016-05-17 15:26:33 +0700

    fix custom validation

--- app/models/danvanthiri_core/custom_validation.rb
+++ app/models/danvanthiri_core/custom_validation.rb
@@ -9,7 +9,7 @@ module DanvanthiriCore
     def integer_field
       self.class.columns.each do |column|
         value = self.send(column.name)
-        if !value.blank? && column.type.to_s=="integer" && value > 2147483647
+        if !value.blank? && column.type.to_s=="integer" && value.to_i > 2147483647
           errors.add column.name.to_sym, "integer value out of range"
         end
       end
