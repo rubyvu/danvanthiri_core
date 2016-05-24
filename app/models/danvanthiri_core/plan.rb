3722965b9adc7355666e8ea7d25a857844879bc0
Ngoc Vu <ngoc@digifutura.com>
2016-05-24 11:18:40 +0700

    mount uploader for plan image

--- app/models/danvanthiri_core/plan.rb
+++ app/models/danvanthiri_core/plan.rb
@@ -3,5 +3,10 @@ module DanvanthiriCore
     has_many :doctors_plans, foreign_key: "plan_id"
     has_many :doctors, through: :doctors_plans
     include CustomValidation
+
+    validates :name, presence: true
+    validates :unit_price, presence: true
+    
+    mount_uploader :image, ImageUploader
   end
 end
