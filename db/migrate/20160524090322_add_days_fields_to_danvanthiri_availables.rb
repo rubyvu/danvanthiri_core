604e56765d921f48db905c9b59cd0ef629bac53f
Ngoc Vu <ngoc@digifutura.com>
2016-05-24 16:26:52 +0700

    edit migration

--- db/migrate/20160524090322_add_days_fields_to_danvanthiri_availables.rb
+++ db/migrate/20160524090322_add_days_fields_to_danvanthiri_availables.rb
@@ -1,11 +1,11 @@
 class AddDaysFieldsToDanvanthiriAvailables < ActiveRecord::Migration
   def change
-    add_column :danvanthiri_availables, :sunday, :boolean
-    add_column :danvanthiri_availables, :monday, :boolean
-    add_column :danvanthiri_availables, :tuesday, :boolean
-    add_column :danvanthiri_availables, :wednesday, :boolean
-    add_column :danvanthiri_availables, :thursday, :boolean
-    add_column :danvanthiri_availables, :friday, :boolean
-    add_column :danvanthiri_availables, :saturday, :boolean
+    add_column :danvanthiri_core_availables, :sunday, :boolean
+    add_column :danvanthiri_core_availables, :monday, :boolean
+    add_column :danvanthiri_core_availables, :tuesday, :boolean
+    add_column :danvanthiri_core_availables, :wednesday, :boolean
+    add_column :danvanthiri_core_availables, :thursday, :boolean
+    add_column :danvanthiri_core_availables, :friday, :boolean
+    add_column :danvanthiri_core_availables, :saturday, :boolean
   end
 end
