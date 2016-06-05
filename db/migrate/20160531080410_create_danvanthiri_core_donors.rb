6845c195db09e18b339519dd49ff16060de012eb
Ruby Vu <rubyvu.vn@gmail.com>
2016-06-05 14:36:26 +0700

    edit migration

--- db/migrate/20160531080410_create_danvanthiri_core_donors.rb
+++ db/migrate/20160531080410_create_danvanthiri_core_donors.rb
@@ -2,10 +2,11 @@ class CreateDanvanthiriCoreDonors < ActiveRecord::Migration
   def change
     create_table :danvanthiri_core_donors do |t|
       t.integer :patient_id
-      t.string :category
-      t.string :sub_category
-      t.string :blood_group
-      t.datetime :date_of_birth
+      t.integer :category
+      t.integer :sub_category
+      t.integer :blood_group
+      t.integer :age
+      t.integer :bmi
 
       t.timestamps null: false
     end
--- db/migrate/20160603075121_add_fields_to_danvanthiri_core_donors.rb
+++
@@ -1,6 +0,0 @@
-class AddFieldsToDanvanthiriCoreDonors < ActiveRecord::Migration
-  def change
-    add_column :danvanthiri_core_donors, :bmi, :string
-    add_column :danvanthiri_core_donors, :age, :integer
-  end
-end
