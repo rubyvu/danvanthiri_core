89c4917b446b3d9164948499eb580ad77022cba6
pcuong <pcuongnn@gmail.com>
2016-06-07 15:01:10 +0700

    mount_uploader icon of pharmacy categories

--- app/models/danvanthiri_core/pharmacy_category.rb
+++ app/models/danvanthiri_core/pharmacy_category.rb
@@ -2,5 +2,7 @@ module DanvanthiriCore
   class PharmacyCategory < ActiveRecord::Base
     validates :name, presence: true, uniqueness: true
     belongs_to :patient
+    
+    mount_uploader :icon, ImageUploader
   end
 end
