597d791d9fc9f661ea8fb6e5396271f2fd0ea02b
pcuong <pcuongnn@gmail.com>
2016-06-08 15:49:52 +0700

    remove foreign_key

--- app/models/danvanthiri_core/pharmacy_category.rb
+++ app/models/danvanthiri_core/pharmacy_category.rb
@@ -1,6 +1,6 @@
 module DanvanthiriCore
   class PharmacyCategory < ActiveRecord::Base
-    has_and_belongs_to_many :pharmacies, foreign_key: "pharmacy_category_id"
+    has_and_belongs_to_many :pharmacies
     validates :name, presence: true, uniqueness: true
 
     mount_uploader :icon, ImageUploader
