b14824c2dc3f678eb3594f0670e33228adcbf000
pcuong <pcuongnn@gmail.com>
2016-06-08 15:10:16 +0700

    remove index

--- db/migrate/20160608070010_create_join_table_pharmacy_category_pharmacy.rb
+++ db/migrate/20160608070010_create_join_table_pharmacy_category_pharmacy.rb
@@ -1,8 +1,7 @@
 class CreateJoinTablePharmacyCategoryPharmacy < ActiveRecord::Migration
   def change
     create_join_table :danvanthiri_core_pharmacy_categories, :danvanthiri_core_pharmacies do |t|
-      t.index [:pharmacy_category_id, :pharmacy_id], name: "index_on_pharmacy_category_id_and_pharmacy_id"
-      t.index [:pharmacy_id, :pharmacy_category_id], name: "index_on_pharmacy_id_and_pharmacy_category_id"
+  
     end
   end
 end
