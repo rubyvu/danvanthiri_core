cf0e85b4a37ab9c8662d3f3a938dee7af05be872
pcuong <pcuongnn@gmail.com>
2016-06-08 21:12:47 +0700

    re-create association has_many through

--- app/models/danvanthiri_core/pharmacy.rb
+++ app/models/danvanthiri_core/pharmacy.rb
@@ -1,6 +1,7 @@
 module DanvanthiriCore
   class Pharmacy < ActiveRecord::Base
-    has_and_belongs_to_many :pharmacy_categories, foreign_key: "danvanthiri_core_pharmacy_id"
+    has_many :pharmacy_category_pharmacies, foreign_key: "pharmacy_id"
+    has_many :pharmacy_categories, through: :pharmacy_category_pharmacies
     validates :name, presence: true, uniqueness: true
   end
 end
