93001297ef90ac28c030049a056c20a4cbea1ce9
Ngoc Vu <ngoc@digifutura.com>
2016-06-13 10:17:21 +0700

    edit migration

--- db/migrate/20160613023922_create_danvanthiri_core_hospitals_hospital_categories.rb
+++ db/migrate/20160613023922_create_danvanthiri_core_hospitals_hospital_categories.rb
@@ -1,7 +1,8 @@
 class CreateDanvanthiriCoreHospitalsHospitalCategories < ActiveRecord::Migration
   def change
     create_table :danvanthiri_core_hospitals_hospital_categories do |t|
-
+      t.integer :hospital_id
+      t.integer :hospital_category_id
       t.timestamps null: false
     end
   end
