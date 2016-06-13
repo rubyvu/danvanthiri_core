f4e0f8f8d7be870c17edcce3f2060ee2a2675ae2
Ngoc Vu <ngoc@digifutura.com>
2016-06-13 13:32:36 +0700

    rebuild pharmacies table - pharmacy address helpers, availables

--- app/models/danvanthiri_core/pharmacy.rb
+++ app/models/danvanthiri_core/pharmacy.rb
@@ -5,10 +5,75 @@ module DanvanthiriCore
     has_many :likes, as: :likeable, dependent: :destroy
     has_many :ratings, as: :rateable, dependent: :destroy
 
+    has_many :availables, as: :owner, dependent: :destroy
+
     validates :name, presence: true, uniqueness: true
+    validate :address_validate
+
+    mount_uploader :logo, ImageUploader
+
+    attr_accessor :update_location_field
 
     def update_rating!
       update_column :rate, ratings.average(:rate)
+    end
+
+    def available_ranges(date=nil)
+      avail_on_date = availables
+      if date
+        wday = date.strftime("%A").downcase
+        avail_on_date = availables.send(wday)
+      end
+      availables.map(&:json_details)
+    end
+
+    def addr
+      [self.addr_street, self.addr_area, self.addr_city, self.addr_state].reject{|x| x.blank?}.join(', ')
+    end
+
+    def get_lat
+      unless lat
+        update_location_by_address
+        self.save
+      end
+      lat
+    end
+
+    def get_lng
+      unless lng
+        update_location_by_address
+        self.save
+      end
+      lng
+    end
+
+    def update_location_by_address
+      g=Geokit::Geocoders::GoogleGeocoder.geocode addr
+      self.lat = g.lat
+      self.lng = g.lng
+    end
+
+    def update_location_by_latlng
+      g=Geokit::Geocoders::GoogleGeocoder.geocode "#{self.lat},#{self.lng}"
+      self.addr_street = g.street_name
+      self.addr_area = g.district
+      self.addr_city = g.city
+      self.addr_state = g.state_name
+    end
+
+    before_validation :check_address_change
+    private
+    def check_address_change
+      if self.update_location_field=='lat-lng'
+        update_location_by_latlng
+      else
+        update_location_by_address if addr_street_changed? || addr_area_changed? || addr_city_changed? || addr_state_changed?
+      end
+    end
+    def address_validate
+      if (addr_street.blank? || addr_city.blank? || addr_state.blank?) && (lat.blank? & lng.blank?)
+        errors.add :address, "or latitude-longitude must be provided"
+      end
     end
   end
 end
---
+++ db/migrate/20160613061114_rebuild_danvanthiri_core_pharmacy.rb
@@ -0,0 +1,24 @@
+class RebuildDanvanthiriCorePharmacy < ActiveRecord::Migration
+  def change
+    drop_table :danvanthiri_core_pharmacies
+    create_table :danvanthiri_core_pharmacies do |t|
+      t.string :name
+      t.string :email
+      t.string :logo
+      t.string :mobile_number
+      t.integer :pharmacy_category_id
+      t.boolean :home_delivery
+      t.string :certification
+      t.string :license
+      t.text :about
+      t.string :addr_street
+      t.string :addr_area
+      t.string :addr_city
+      t.string :addr_state
+      t.float :lat
+      t.float :lng
+
+      t.timestamps null: false
+    end
+  end
+end
