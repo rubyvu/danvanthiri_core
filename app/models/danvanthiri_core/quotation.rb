5300058f71b95239d086054b7d703b1f79b7840f
Ngoc Vu <ngoc@digifutura.com>
2016-06-14 11:54:28 +0700

    quotation owner

--- app/models/danvanthiri_core/quotation.rb
+++ app/models/danvanthiri_core/quotation.rb
@@ -1,12 +1,12 @@
 module DanvanthiriCore
   class Quotation < ActiveRecord::Base
-    belongs_to :onwer, polymorphic: true
+    belongs_to :owner, polymorphic: true
     belongs_to :pharmacy
     has_many :quote_items, dependent: :destroy, foreign_key: "quotation_id"
 
     accepts_nested_attributes_for :quote_items, allow_destroy: true
     validates :quote_items, presence: true
-    
+
     mount_uploader :photo, ImageUploader
   end
 end
