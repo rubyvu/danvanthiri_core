d128a7f46aaf18a9659f6ba3ad245360f3149165
Ngoc Vu <ngoc@digifutura.com>
2016-06-22 15:44:55 +0700

    accept nested attribute for order items

--- app/models/danvanthiri_core/medicine_order.rb
+++ app/models/danvanthiri_core/medicine_order.rb
@@ -3,7 +3,9 @@ module DanvanthiriCore
     enum status: [:pending, :processing, :finished]
     belongs_to :owner, polymorphic: true
     belongs_to :orderable, polymorphic: true
-    has_many :order_items, dependent: :destroy, foreign_key: "medicine_order_id"
+    has_many :medicine_order_items, dependent: :destroy, foreign_key: "medicine_order_id"
+
+    accepts_nested_attributes_for :medicine_order_items, allow_destroy: true
 
     validates :orderable, :order_items, :addr_line_1, :addr_city, :addr_state, presence: true
   end
