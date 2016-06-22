c1bc8a072b3adf453900e47b166b81bd7d47913d
Ngoc Vu <ngoc@digifutura.com>
2016-06-22 11:56:26 +0700

    notification for quotations

--- app/models/danvanthiri_core/notification.rb
+++ app/models/danvanthiri_core/notification.rb
@@ -25,26 +25,41 @@ module DanvanthiriCore
       end
     end
 
-    def push_patient(act)
-      patient = target.patient
-      doctor = target.doctor
-      case act
-      when "accepted"
-        message = "Your appointment with #{doctor.name} has been accepted."
-      when "cancelled", "cancelled_by_doctor"
-        message = "Your appointment with #{doctor.name} has been cancelled."
-      when "rejected"
-        message = "Your appointment with #{doctor.name} has been rejected."
-      when "finished"
-        message = "Your appointment with #{patient.name} has been finished."
+    def push_patient(act, target="Appointment")
+      if target=="Appointment"
+        patient = target.patient
+        doctor = target.doctor
+        case act
+        when "accepted"
+          message = "Your appointment with #{doctor.name} has been accepted."
+        when "cancelled", "cancelled_by_doctor"
+          message = "Your appointment with #{doctor.name} has been cancelled."
+        when "rejected"
+          message = "Your appointment with #{doctor.name} has been rejected."
+        when "finished"
+          message = "Your appointment with #{patient.name} has been finished."
+        end
+        update_column :message, message
+        unless patient.gcm_registration.blank?
+          serv = GcmService.new
+          data = {notification_id: id, appointment_id: target_id, status: target.status, message: message}
+          serv.notify(data, [patient.gcm_registration])
+        end
+      elsif target=="Quotation"
+        case act
+        when "response-quote"
+          quoteable = target.quoteable
+          message = "#{quoteable.name} has responded to your quotation request"
+        end
+
+        update_column :message, message
+        unless patient.gcm_registration.blank?
+          serv = GcmService.new
+          data = {notification_id: id, quotation_id: target_id, status: target.status, message: message}
+          serv.notify(data, [patient.gcm_registration])
+        end
       end
 
-      update_column :message, message
-      unless patient.gcm_registration.blank?
-        serv = GcmService.new
-        data = {notification_id: id, appointment_id: target_id, status: target.status, message: message}
-        serv.notify(data, [patient.gcm_registration])
-      end
     end
 
     def read!
--- app/models/danvanthiri_core/quotation.rb
+++ app/models/danvanthiri_core/quotation.rb
@@ -1,13 +1,13 @@
 module DanvanthiriCore
   class Quotation < ActiveRecord::Base
-    enum status: [:pending, :accepted]
+    enum status: [:pending, :responded]
     belongs_to :owner, polymorphic: true
     belongs_to :quoteable, polymorphic: true
     has_many :quote_items, dependent: :destroy, foreign_key: "quotation_id"
 
     accepts_nested_attributes_for :quote_items, allow_destroy: true
     validates :quote_items, presence: true
-  
+
     mount_uploader :photo, ImageUploader
 
     def update_price!
