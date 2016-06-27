37967c6c09e650bb6f3a4bdd9fbaef33515f40f2
Luc <luc.dangquang@rainmaker-labs.com>
2016-06-27 16:24:36 +0700

    add delivery_time, create notifications_relations, validate_delivery_when_status_is_accepted

--- app/models/danvanthiri_core/medicine_order.rb
+++ app/models/danvanthiri_core/medicine_order.rb
@@ -1,12 +1,20 @@
 module DanvanthiriCore
   class MedicineOrder < ActiveRecord::Base
     enum status: [:pending, :accepted, :finished, :cancelled_by_patient, :cancelled_by_pharmacy, :rejected]
+
     belongs_to :owner, polymorphic: true
     belongs_to :orderable, polymorphic: true
     has_many :medicine_order_items, dependent: :destroy, foreign_key: "medicine_order_id"
+    has_many :notifications, as: :target
 
     accepts_nested_attributes_for :medicine_order_items, allow_destroy: true
 
     validates :orderable, :medicine_order_items, :addr_line_1, :addr_city, :addr_state, presence: true
+    validates :delivery_time, presence: true, if: :status_is_accepted?
+
+    def status_is_accepted?
+      accepted?
+    end
+
   end
 end
---
+++ db/migrate/20160627091856_add_delivery_time_to_danvanthiri_core_medicine_orders.rb
@@ -0,0 +1,4 @@
+class AddDeliveryTimeToDanvanthiriCoreMedicineOrders < ActiveRecord::Migration
+  def change
+  end
+end
