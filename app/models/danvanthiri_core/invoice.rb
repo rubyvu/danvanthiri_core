7d7ff31ceee8bd112263524e14487a2eb7b02b4b
Luc <luc.dangquang@rainmaker-labs.com>
2016-06-30 17:08:18 +0700

    update invoice

--- app/models/danvanthiri_core/invoice.rb
+++ app/models/danvanthiri_core/invoice.rb
@@ -2,5 +2,8 @@ module DanvanthiriCore
   class Invoice < ActiveRecord::Base
     has_many :invoice_items, dependent: :destroy
     accepts_nested_attributes_for :invoice_items, allow_destroy: true
+
+
+    validates :email, uniqueness: true
   end
 end
--- app/models/danvanthiri_core/invoice_item.rb
+++ app/models/danvanthiri_core/invoice_item.rb
@@ -2,5 +2,10 @@ module DanvanthiriCore
   class InvoiceItem < ActiveRecord::Base
     belongs_to :invoice
     belongs_to :plan
+
+    validates :price, presence: true
+    validates :quantity, :numericality => { :greater_than => 0 }
+    validates :plan, presence: true
+    validates :invoice, presence: true
   end
 end
