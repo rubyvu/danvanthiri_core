f971d6c8f7e298c2893f89ee3f3d8a2f14b549d2
Luc <luc.dangquang@rainmaker-labs.com>
2016-06-30 11:44:11 +0700

    fixed error when drop table, prefix danvanthiri_core

--- db/migrate/20160630041851_remove_doctor_orders_line_items.rb
+++ db/migrate/20160630041851_remove_doctor_orders_line_items.rb
@@ -1,6 +1,6 @@
 class RemoveDoctorOrdersLineItems < ActiveRecord::Migration
   def change
-    drop_table :doctor_orders
-    drop_table :doctor_line_items
+    drop_table :danvanthiri_core_doctor_orders
+    drop_table :danvanthiri_core_doctor_line_items
   end
 end
