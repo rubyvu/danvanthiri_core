0425190c9d1b246cbda70102b3b1464eabe7dd12
Ngoc Vu <ngoc@digifutura.com>
2016-06-30 15:17:26 +0700

    lab_categoru has many labs

--- app/models/danvanthiri_core/lab_category.rb
+++ app/models/danvanthiri_core/lab_category.rb
@@ -1,5 +1,6 @@
 module DanvanthiriCore
   class LabCategory < ActiveRecord::Base
+    has_many :labs, foreign_key: "lab_category_id", dependent: :destroy
     validates :name, presence: true
     validates :name, uniqueness: true
     mount_uploader :logo, ImageUploader
