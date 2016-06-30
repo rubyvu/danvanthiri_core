c757537c53c3e987732d10a7bb5ab9cff4558b45
Luc <luc.dangquang@rainmaker-labs.com>
2016-06-30 10:55:14 +0700

    update belongs_to, accept_nested for line_item

--- app/models/danvanthiri_core/doctor_line_item.rb
+++ app/models/danvanthiri_core/doctor_line_item.rb
@@ -2,5 +2,6 @@ module DanvanthiriCore
   class DoctorLineItem < ActiveRecord::Base
     belongs_to :doctor_order
     belongs_to :doctor_plan
+    belongs_to :plan
   end
 end
--- app/models/danvanthiri_core/doctor_order.rb
+++ app/models/danvanthiri_core/doctor_order.rb
@@ -1,5 +1,6 @@
 module DanvanthiriCore
   class DoctorOrder < ActiveRecord::Base
     has_many :doctor_line_items, dependent: :destroy
+    accepts_nested_attributes_for :doctor_line_items, allow_destroy: true
   end
 end
