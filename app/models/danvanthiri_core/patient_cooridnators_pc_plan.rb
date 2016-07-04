5a755f94016b36ba2be09534ba7249e089a6f28f
pcuong <pcuongnn@gmail.com>
2016-07-04 14:43:49 +0700

    association between patient coordinator & plan

--- app/models/danvanthiri_core/patient_cooridnators_pc_plan.rb
+++ app/models/danvanthiri_core/patient_cooridnators_pc_plan.rb
@@ -1,5 +1,7 @@
 module DanvanthiriCore
   class PatientCoordinatorsPcPlan < ActiveRecord::Base
     enum status:  [:pending, :active, :expired]
+    belongs_to :patient_coordinator
+    belongs_to :pc_plan
   end
 end
