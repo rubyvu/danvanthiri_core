aba1737284b2a6403bb11c0725acec47360e28bf
Luc <luc.dangquang@rainmaker-labs.com>
2016-07-05 11:55:11 +0700

    update add complted

--- db/migrate/20160705041246_add_completed_at_status_to_danvanthiri_core_invoices.rb
+++ db/migrate/20160705041246_add_completed_at_status_to_danvanthiri_core_invoices.rb
@@ -1,6 +1,5 @@
 class AddCompletedAtStatusToDanvanthiriCoreInvoices < ActiveRecord::Migration
   def change
     add_column :danvanthiri_core_invoices, :status, :integer, default: true
-    add_column :danvanthiri_core_invoices, :completed_at, :datetime, default: nil
   end
 end
