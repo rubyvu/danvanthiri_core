02bc6d800ddd108b45398ecf878be9593217a992
pcuong <pcuongnn@gmail.com>
2016-07-05 11:26:08 +0700

    remove pc_plan

--- app/models/danvanthiri_core/patient_cooridnators_pc_plan.rb
+++
@@ -1,7 +0,0 @@
-module DanvanthiriCore
-  class PatientCoordinatorsPcPlan < ActiveRecord::Base
-    enum status:  [:pending, :active, :expired]
-    belongs_to :patient_coordinator
-    belongs_to :pc_plan
-  end
-end
