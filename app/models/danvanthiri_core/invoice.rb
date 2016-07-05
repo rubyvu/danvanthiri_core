84681ff2624d585b85689c9ac75844b83ccfc3fe
Luc <luc.dangquang@rainmaker-labs.com>
2016-07-05 11:10:56 +0700

    update invoice_model enum status and completed_at

--- app/models/danvanthiri_core/invoice.rb
+++ app/models/danvanthiri_core/invoice.rb
@@ -3,5 +3,20 @@ module DanvanthiriCore
     has_many :invoice_items, dependent: :destroy
     accepts_nested_attributes_for :invoice_items, allow_destroy: true
     validates :email, uniqueness: true
+    validates :invoice_items, presence: true
+
+    enum status [:pending, :completed, :failed]
+
+    def mark_complete
+      self.status = :completed
+      self.save
+      touch :completed_at
+    end
+
+    def mark_fail
+      self.status = :failed
+      self.save
+    end
+
   end
 end
