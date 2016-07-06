b6fc202714f73a7c05b38b9a146c948d24787153
Ngoc Vu <ngoc@digifutura.com>
2016-07-06 11:07:09 +0700

    pharmacy activities

--- app/models/danvanthiri_core/pharmacy.rb
+++ app/models/danvanthiri_core/pharmacy.rb
@@ -10,6 +10,7 @@ module DanvanthiriCore
     has_many :availables, as: :owner, dependent: :destroy
     has_many :quotations, as: :quoteable, dependent: :destroy
     has_many :medicine_orders, as: :orderable, dependent: :destroy
+    has_many :activities, as: :owner, dependent: :destroy
 
     validates :name, presence: true, uniqueness: true
     validates :pharmacy_category_id, presence: true, on: :update
