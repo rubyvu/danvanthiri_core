8c5849935ca6411ada266e7a7798971758088db9
Luc <luc.dangquang@rainmaker-labs.com>
2016-07-06 10:21:09 +0700

    update status

--- db/migrate/20160706030124_change_status_default.rb
+++ db/migrate/20160706030124_change_status_default.rb
@@ -1,11 +1,5 @@
 class ChangeStatusDefault < ActiveRecord::Migration
   def change
-    def up
-      change_column_default(:danvanthiri_core_invoices, :status, 0)
-    end
-
-    def down
-      change_column_default(:danvanthiri_core_invoices, :status, nil)
-    end
+    change_column_default(:danvanthiri_core_invoices, :status, 0)
   end
 end
