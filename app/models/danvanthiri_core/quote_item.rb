9020e3654251b3ea9b0e6319be3479a82160e1bc
Ngoc Vu <ngoc@digifutura.com>
2016-07-07 09:47:30 +0700

    validate quote item price

--- app/models/danvanthiri_core/quote_item.rb
+++ app/models/danvanthiri_core/quote_item.rb
@@ -4,6 +4,7 @@ module DanvanthiriCore
 
     validates :name, presence: true
     validates :price, presence: true, on: :update
+    validates :price, numericality: { only_integer: true }, allow_blank: true
     include CustomValidation
   end
 end
