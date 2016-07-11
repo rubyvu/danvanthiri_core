434b2ee2e004b2a8852ebba4a25d90f171e1d1c1
Luc <luc.dangquang@rainmaker-labs.com>
2016-07-11 10:47:23 +0700

    update

--- app/models/danvanthiri_core/ambulance_service.rb
+++ app/models/danvanthiri_core/ambulance_service.rb
@@ -9,8 +9,8 @@ module DanvanthiriCore
     has_many :ambulance_services_categories, foreign_key: "ambulance_service_id", dependent: :destroy
     has_many :ambulance_categories, through: :ambulance_services_categories
 
-    validates :name, :area, :logo, :ambulance_categories, :mobile, presence: true
-    validates :mobile, length: { is: 10 }, numericality: true, uniqueness: true
+    validates :name, :area, :logo, :ambulance_categories, presence: true
+    validates :mobile, length: { is: 10 }, numericality: true, presence: true, uniqueness: true
     validates :year_of_establishment, numericality: {less_than_or_equal_to: Time.now.year}
 
     def update_rating!
