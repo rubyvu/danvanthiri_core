777b3aa4c323bbf00882d0373206706b6e920ef6
Ngoc Vu <ngoc@digifutura.com>
2016-07-11 09:36:43 +0700

    notification for pharmacy

--- app/models/danvanthiri_core/notification.rb
+++ app/models/danvanthiri_core/notification.rb
@@ -27,6 +27,26 @@ module DanvanthiriCore
       push_ios(data) unless doctor.ios_device_token.blank?
     end
 
+    def push_pharmacy(act)
+      patient = target.patient
+
+      case act
+        when "book"
+          message = "#{patient.name} requested you for new order."
+        when "cancelled", "cancelled_by_patient"
+          message = "#{patient.name} has cancelled an order."
+      end
+
+      update_column :message, message
+      data = {notification_id: id, appointment_id: target_id, status: target.status, message: message}
+      unless owner.gcm_registration.blank?
+        serv = GcmService.new
+        serv.notify(data, [owner.gcm_registration])
+      end
+
+      push_ios(data) unless owner.ios_device_token.blank?
+    end
+
     def push_pc(act)
       patient = target.patient
 
