73986ba115c0080fb4b00a4aa8caa0f2bfa49af0
Ngoc Vu <ngoc@digifutura.com>
2016-07-11 09:55:59 +0700

    push notification for pharmacy quotation request

--- app/models/danvanthiri_core/notification.rb
+++ app/models/danvanthiri_core/notification.rb
@@ -27,23 +27,52 @@ module DanvanthiriCore
       push_ios(data) unless doctor.ios_device_token.blank?
     end
 
-    def push_pharmacy(act)
+    def push_pharmacy(act, obj_type="Appointment")
       patient = target.patient
+      data = {}
+      if obj_type=='Appointment'
+        case act
+          when "book"
+            message = "#{patient.name} requested you for new order."
+          when "cancelled", "cancelled_by_patient"
+            message = "#{patient.name} has cancelled an order."
+        end
+        update_column :message, message
+        data = {notification_id: id, appointment_id: target_id, status: target.status, message: message}
 
-      case act
-        when "book"
-          message = "#{patient.name} requested you for new order."
-        when "cancelled", "cancelled_by_patient"
-          message = "#{patient.name} has cancelled an order."
+      elsif obj_type=='Quotation'
+        data = {notification_id: id, status: target.status}
+        case act
+        when "request"
+            patient = target.owner
+            message = "#{quoteable.name} has responded to your quotation request"
+            data[:quotation_id] = target_id
+            data[:quotation_target] = target.quoteable_type.split("::").last
+          when "response-quotes"
+            sample = target.quotations.last
+            type = sample.quoteable_type.split('::').last
+            quotes = target.quotations.responded
+            if quotes.blank?
+              message = "No #{type.downcase} respond to your quotation"
+            elsif quotes.count == 1
+              message = "#{type} #{sample.quoteable.name} have responded to your quote request"
+            else
+              others = quotes.count-1
+              service_name = others == 1 ? type.downcase : "#{type.downcase}s"
+              message = "#{type} #{sample.quoteable.name} and #{others} other #{service_name} have responded to your quote request"
+            end
+            data[:quote_thread_id] = target_id
+            data[:quotation_target] = sample.quoteable_type.split("::").last
+        end
+
+        update_column :message, message
+        data[:message] = message
       end
 
-      update_column :message, message
-      data = {notification_id: id, appointment_id: target_id, status: target.status, message: message}
       unless owner.gcm_registration.blank?
         serv = GcmService.new
         serv.notify(data, [owner.gcm_registration])
       end
-
       push_ios(data) unless owner.ios_device_token.blank?
     end
 
