2d56c62dac7c18d7029fe4fa3bf5eebbfc358a31
Luc <luc.dangquang@rainmaker-labs.com>
2016-07-12 08:50:19 +0700

    mising module DanvanthiriCore

--- app/models/danvanthiri_core/task_event.rb
+++ app/models/danvanthiri_core/task_event.rb
@@ -1,12 +1,14 @@
-class TaskEvent < ActiveRecord::Base
-  enum task_event_type: [:task, :event]
-  before_validation :set_task_event_datetime
-  attr_accessor :date_str, :time_str
+module DanvanthiriCore
+  class TaskEvent < ActiveRecord::Base
+    enum task_event_type: [:task, :event]
+    before_validation :set_task_event_datetime
+    attr_accessor :date_str, :time_str
 
-  belongs_to :doctor
-  validates :date_str, :name, :start_datetime, :description, :task_event_type, presence: true
+    belongs_to :doctor
+    validates :date_str, :name, :start_datetime, :description, :task_event_type, presence: true
 
-  def set_task_event_datetime
-    self.start_datetime = DateTime.parse "#{date_str} #{time_str}" if date_str.present?
+    def set_task_event_datetime
+      self.start_datetime = DateTime.parse "#{date_str} #{time_str}" if date_str.present?
+    end
   end
 end
